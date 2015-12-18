5e6f5721aa5205a53d698f01d64a7c11c8d279e8
Cortney Lane <cdlane124@gmail.com>
2015-12-17 21:01:05 -0800

    improve JS functionality, search results generation, add about.html

---
+++ script.js
@@ -0,0 +1,56 @@
+
+// MAP DISPLAY ***********************
+var map;
+var infowindow;
+function initMap() {
+  var CodeFellows = {lat: 47.6235481, lng: -122.33621199999999};
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: CodeFellows,
+    zoom: 15
+  });
+  var marker=new google.maps.Marker({
+    position:CodeFellows,
+    icon:'images/cf.png'
+  });
+
+  marker.setMap(map);
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: CodeFellows,
+    radius: 500,
+    types: ['restaurant', 'cafe','bakery', 'meal_takeaway', 'food', 'point_of_interest','bar'],
+    openNow: true,
+    maxPriceLevel: 1
+    // Can add maxPrice: 1,2,3,etc. via the 'submit' event listener? MaxPriceLevel:1-4
+  }, callback);
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+
+// BEGIN code that's gonna get replaced
+
+  $('.restaurants').append('<tr><td>'+ (i + 1) + ' : ' +results[i].name + ' ' + results[i].vicinity + '</td></tr>');
+    // $('.restaurants').append('<td>'+ results[i].vicinity +'</td></tr>');
+
+// END code that's gonna get replaced
+    }
+  }
+}
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    var nameLocation = place.name + ' ' +place.vicinity + " Rated" + place.rating;
+    infowindow.setContent(nameLocation);
+    // infowindow.setContent(place.vicinity);
+    infowindow.open(map, this);
+  });
+}
